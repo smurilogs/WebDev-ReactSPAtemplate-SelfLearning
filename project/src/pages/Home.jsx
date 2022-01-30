e6831aa72a0ab970bb69fd824758277f3c4b9599
smurilogs <smurilogs@gmail.com>
2022-01-30 09:35:04 -0300

    tiny adjustments to make home page clean

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -6,43 +6,9 @@ import './Home.css';
 function Home(props) {
 
 	return (
-    <div className="Home">
-    <div id="app">
+        <div className="Home">
 
-        <div id="sidebar" className='active'>
-            <div className="sidebar-wrapper active">
-                <div className="sidebar-menu">
-                    
-                    <ul className="menu">
-                
-                        <li className='sidebar-title'>Submenu 1</li>
-                
-                        <li className="sidebar-item active ">
-                            <a href="index.html" className='sidebar-link'>
-                                <span>Item 1</span>
-                            </a>
-                        </li>
-
-                        <li className="sidebar-item ">
-                            <a href="index.html" className='sidebar-link'>
-                                <span>Item 2</span>
-                            </a>
-                        </li>
-
-                        <li className='sidebar-title'>Submenu 2</li>
-                
-                        <li className="sidebar-item ">
-                            <a href="form-layout.html" className='sidebar-link'>
-                                <span>Item 1</span>
-                            </a>
-                        </li>
-                    </ul>
-                
-                </div>
-            </div>
         </div>
-    </div>
-    </div>
     );
 }
 
